99c5aeb228874b2e5dad009caef2e996412fce7b
Carolyn Davis <carolyn.m.davis01@gmail.com>
2021-07-13 10:28:45 -0500

    finished bonuses for function exercises

--- python-exercises/function_exercises.py
+++ python-exercises/function_exercises.py
@@ -96,10 +96,10 @@ apply_discount(6,15)
 # contains commas in it as input, and return a number as output.
 # =============================================================================
 
-def handle_commas(user_input):
+def handle_commas(user_input):  #function created to handle any input from call function
     # if "," in user_input:
-    return user_input.replace(',', '')
-    
+    return user_input.replace(',', '')  #returns any user input like 1,000 inputted as a string
+                                        #replace() method replaces comma with a space
 handle_commas("1,000")
 
 # =============================================================================
@@ -107,15 +107,15 @@ handle_commas("1,000")
 # letter grade associated with that number (A-F).
 # =============================================================================
 
-def get_letter_grade(number):
-    grade_book = [{"A" : [i for i in range(90, 101)]}, 
-                  {"B" : [i for i in range(80, 90)]},
-                  {"C" : [i for i in range(70, 80)]},
+def get_letter_grade(number):    #runs any argument, such as numeric value as stated in parentheses
+    grade_book = [{"A" : [i for i in range(90, 101)]},  #for loop runs the argument through a range of numbers starting
+                  {"B" : [i for i in range(80, 90)]},   #--> starting with the first number and stopping but not including the last #
+                  {"C" : [i for i in range(70, 80)]},   #from 0 up to but not including 101
                   {"F" : [i for i in range(0, 70)]}]
-    for grades in grade_book:
-        for letter, values in grades.items():
+    for grades in grade_book:      #this for loop runs the qualifying argument above
+        for letter, values in grades.items(): #if letter aligns with a value in the list
             if number in values:
-                return letter
+                return letter  #return the letter corresponding with numeric input
         
                   
   
@@ -129,10 +129,10 @@ get_letter_grade(55)
 # =============================================================================
 
 def remove_vowels(words):
-    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-    words = list(words)
-    words = [letter for letter in words if letter not in vowels]
-    return "".join(words)
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']   #list specifying what the function should avoid
+    words = list(words) #runs the argument as a list
+    words = [letter for letter in words if letter not in vowels] #if the argument is not in the vowels list...
+    return "".join(words) #join the consonant letters in the words not containing vowels/ 
 
 remove_vowels('pooopoloigicalallly')
 
@@ -154,9 +154,9 @@ remove_vowels('pooopoloigicalallly')
 # x = x.split("_")[1] 
 
 def normalize_name(words):
-    words = words.lower()
-    words = words.strip()
-    words = words.replace(' ', '_') 
+    words = words.lower()           #strings inputted will be made lower case
+    words = words.strip()           #strings inputted will be stripped of white space
+    words = words.replace(' ', '_') #strings inputted with a space will be replaceed with underscore
     return words
 
 normalize_name('bagle Stuff ')
@@ -166,15 +166,75 @@ normalize_name('bagle Stuff ')
 # Write a function named cumulative_sum that accepts a list of numbers and returns a 
 # list that is the cumulative sum of the numbers in the list.
 # =============================================================================
-import pandas as pd
+import pandas as pd                #imports math library 
 
-def cumulative_sum(num_list):
+def cumulative_sum(num_list):       #expect a collection of nums
     
-    num_list = pd.Series(num_list)
-    num_list = num_list.cumsum()
+    num_list = pd.Series(num_list)  #numbers inoutted should present as a series
+    num_list = num_list.cumsum()    #numbers inputted will be added together cdumulatively
     
     return num_list
 
     
 cumulative_sum([1,1,1]) 
+
+# =============================================================================
+#                                 BONUS
+# =============================================================================
+
+# =============================================================================
+# Create a function named twelveto24. It should accept a string in the format 10:45am or 
+# 4:30pm and return a string that is the representation of the time in a 24-hour format.
+#  Bonus write a function that does the opposite.
+# =============================================================================
+
+def twelveto24(str1):   #argument will be a string
+      
+    # Checking if last two elements of time
+    # is AM and first two elements are 12
+    if str1[-2:] == "AM" and str1[:2] == "12":   #checking to see if last elements are AM and time 12
+        return "00" + str1[2:-2]  #return 00 in place of index 1200am to 0000 PM
+          
+    # remove the AM    
+    elif str1[-2:] == "AM": #if the string contains AM return everything except AM at position -2
+        return str1[:-2] #remove the AM if present
+      
+    # Checking if last two elements of time
+    # is PM and first two elements are 12   
+    elif str1[-2:] == "PM" and str1[:2] == "12": #checking to see if PM is apart of last two elements in argument and 12 at position 2
+        return str1[:-2] #returns elements that meet these conditions
+          
+    else:
+          
+        # add 12 to hours and remove PM
+        return str(int(str1[:2]) + 12) + str1[2:5]  #if conditions present return a string that 
+                                                     #has had 12 hours added to hour inputted with removal of PM
+# Driver Code        
+print(twelveto24("11:00AM"))
+
+
+# =============================================================================
+# Create a function named col_index. It should accept a spreadsheet column name, 
+# and return the index number of the column.
+# col_index('A') returns 1
+# col_index('B') returns 2
+# col_index('AA') returns 27
+# =============================================================================
+
+def col_index(index_column):
+    column_list = {"A": 1, "B": 2, "AA": 27}
+    index_column = column_list[index_column]
+    # for index in column_name:
+    #     for number, in values in numbers.items():
+    #         if column_name in values:
+    #             return column_name
     
+    return index_column
+
+
+col_index('AA')
+
+
+
+
+
